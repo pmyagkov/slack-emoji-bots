81002b252f720d19fcf24c62246c21c3e23a80f8
Pavel Myagkov <p.myagkov@corp.mail.ru>
2016-01-14 19:45:46 +0300

    Greetings and bye

--- index.js
+++ index.js
@@ -8,14 +8,48 @@ var bot = new SlackBot({
   token: BOTS_HASH['joy'],
   name: 'Дух Радости'
 });
+var BOT_STATS = {
+  icon_url: 'https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2015-07-21/7994648307_3d28e08b8cc29e28cef9_48.jpg',
+  attachments: [
+    { image_url: 'https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2015-07-21/7994648307_3d28e08b8cc29e28cef9_48.jpg'}
+  ]
+};
+
+var CHANNEL_ID = 'G0DED7GRJ';
+
+function greetings() {
+  return bot.postTo(CHANNEL_ID, 'Привет, котятки! Я снова с вами.', BOT_STATS);
+}
+
+function bye() {
+  return bot.postTo(CHANNEL_ID, 'Моя работа здесь закончена. До следующего раза, пока!', BOT_STATS);
+}
+
+function count(i, upper, channel) {
+  bot.postTo(channel, '' + i, BOT_STATS);
+
+  if (i + 1 <= upper) {
+
+    setTimeout(function () {
+      count(i + 1, upper, channel);
+    }, 2000);
+  }
+
+}
+
+process.on('SIGINT', function() {
+  bye().then(process.exit, process.exit);
+});
+
+greetings();
 
 bot.on('message', function(data) {
   console.log(data);
 
   if (data.type === 'message' && data.subtype !== 'bot_message') {
-    if (data.text.indexOf('радость') > -1) {
-      bot.postMessageToChannel('general', 'Есть такая эмоция – радость!', {
-        icon_url: 'https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2015-07-21/7994648307_3d28e08b8cc29e28cef9_48.jpg'
+    if (/^count bitch!$/.test(data.text)) {
+      setTimeout(function () {
+        count(1, 3, data.channel);
       });
     }
   }
